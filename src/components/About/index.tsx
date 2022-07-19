fd840ec2f2c5cb3e555f41d420388dd8a71e0259
gaqno <gabriel.aquino@outlook.com>
2022-07-19 19:14:08 -0300

    feat: about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,10 +1,58 @@
+import { faAngular, faCss3, faDocker, faGitAlt, faHtml5, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Loader from "react-loaders";
+import Typewriter from "typewriter-effect";
 import './index.scss'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className='about-page'>
+        <div className='text-zone'>
+          <Typewriter
+              onInit={(typewritter) => {
+                typewritter
+                  .pauseFor(1000)
+                  .changeDeleteSpeed(5)
+                  .changeDelay(1)
+                  .typeString('Welcome!')
+                  .callFunction(() => (<h1> Sobre mim: </h1>))
+                  .deleteAll()
+                  .typeString("Tenho habilidades nos principais frameworks do mercado, proeminentemente frontend.")
+                  .pauseFor(2500)
+                  .deleteAll()
+                  .typeString("Cada dia mais motivado entendendo a grandeza e beleza da programação. ")
+                  .deleteAll()
+                  .typeString("Eiusmod ea proident dolore sunt sunt. Voluptate sint quis incididunt commodo irure anim eu eu eiusmod irure. Cupidatat tempor officia labore duis sunt dolor labore sit mollit quis. Sint quis labore nostrud id non aliqua nostrud commodo reprehenderit pariatur enim cillum adipisicing. Tempor dolor mollit proident nisi cillum elit non duis.")
+                  .deleteAll()
+                  .start()
+              }}
+          />
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faReact} color='#f06529' />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color='#28a4d9' />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faCss3} color='#5ed4f4' />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faHtml5} color='#efd81d' />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color='#ec4d28' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,3 @@
-import { startTransition, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Typewriter from "typewriter-effect";
 
